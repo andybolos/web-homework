80b5a0edc7c433f7ce60295dfb4dd09193ced9fb
Andy Bolos <andybolos@Andys-iMac.local>
2019-09-11 23:24:53 -0600

    feat(transactions): add update mutation

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -21,6 +21,22 @@ const mutation = new GraphQLObjectType({
         return (new TransactionModel({ user_id, description, merchant_id, debit, credit, amount })).save()
       }
     },
+    updateTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString },
+        user_id: { type: GraphQLString },
+        description: { type: GraphQLString },
+        merchant_id: { type: GraphQLString },
+        debit: { type: GraphQLBoolean },
+        credit: { type: GraphQLBoolean },
+        amount: { type: GraphQLFloat }
+      },
+      /* eslint-disable-next-line camelcase */
+      resolve (parentValue, args) {
+        return TransactionModel.findByIdAndUpdate(args.id, args)
+      }
+    },
     removeTransaction: {
       type: TransactionType,
       args: {
