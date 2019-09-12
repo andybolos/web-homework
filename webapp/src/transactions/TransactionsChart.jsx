0051b06b71c8a4ac9a3c743664bd25dba2dd189f
Andy Bolos <andybolos@Andys-iMac.local>
2019-09-11 20:43:39 -0600

    refactor: add legend and styles

--- webapp/src/transactions/TransactionsChart.jsx
+++ webapp/src/transactions/TransactionsChart.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Piechart from 'react-minimal-pie-chart'
+import { css } from '@emotion/core'
+
 export const TransactionsChart = (transactions) => {
   const bakePie = () => {
     const pie = []
@@ -19,6 +21,12 @@ export const TransactionsChart = (transactions) => {
 
   return (
     <>
+      <div css={LegendStyle}>
+        <div>
+          <h4>Credit<span className='LegendColor--credit' /></h4>
+          <h4>Debit<span className='LegendColor--debit' /></h4>
+        </div>
+      </div>
       <Piechart
         data={bakePie()}
         label={({ data, dataIndex }) =>
@@ -28,7 +36,39 @@ export const TransactionsChart = (transactions) => {
           fontSize: '5px',
           fill: '#000000'
         }}
+        style={{ height: '300px' }}
       />
     </>
   )
 }
+
+const LegendStyle = css`
+  margin-top: 56px;
+  margin-bottom: 24px;
+
+  & div:last-child {
+    display: flex;
+  }
+
+  & h4 {
+    display: inline;
+    margin: 0;
+  }
+
+  & span {
+    height: 15px;
+    width: 15px;
+    display: inline-block;
+    margin-left: 8px;
+    margin-right: 16px;
+    border-radius: 50px;
+  }
+
+  & span.LegendColor--credit {
+    background: #DB4939;
+  }
+
+  & span.LegendColor--debit {
+    background: #0ADB92;
+  }
+`
